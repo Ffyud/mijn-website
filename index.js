a8c285a33f9bb197e3dcb48fcbc0486e9a7d36c8
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-04-07 16:24:43 +0200

    html serveren via express

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () =>
-    console.log(`De server doet het op ${PORT}`)
-);
+app.use('/', express.static('public'));
+
+// app.get('/', (req, res) => {
+//     res.send(`yolo`)
+// });
+
+app.listen(PORT, () => {
+    console.log(`De server doet het op ${PORT}`);
+});
